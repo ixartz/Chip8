5b11cab2d1aadcf33e35ff3983c442900efd4828
Rémi Weng <ixartz@users.noreply.github.com>
2013-12-11 00:21:54 +0100

    Add emulate function Chip8 and use constant (constexpr) instead multiple definition

--- src/chip8.cc
+++ src/chip8.cc
@@ -8,9 +8,20 @@ Chip8::~Chip8()
 {
 }
 
+void Chip8::init()
+{
+  pc_ = kmemory_start;
+}
+
+void Chip8::emulate()
+{
+  opcode_ = memory_[pc_] << 8 | memory_[pc_ + 1];
+  pc_ += 2;
+}
+
 void Chip8::load_from_rom(Rom& r)
 {
-  int i = 0x200;
+  int i = kmemory_start;
   rom_ = &r;
 
   while (r >> memory_[i])
--- src/chip8.hh
+++ src/chip8.hh
@@ -6,12 +6,21 @@
 class Chip8
 {
 public:
+  static constexpr unsigned short kmemory_start = 0x200;
+  static constexpr unsigned short kmemory_size = 4096;
+  static constexpr unsigned short knb_register = 16;
+
   Chip8();
   ~Chip8();
-
+  void init();
+  void emulate();
   void load_from_rom(Rom& r);
 private:
-  unsigned char memory_[4096];
+  unsigned short opcode_;
+  unsigned char memory_[kmemory_size];
+  unsigned char V_[knb_register];
+  unsigned short I_;
+  unsigned short pc_;
 
   Rom* rom_;
 };
